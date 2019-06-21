16705bc0358b978c76e67e50ed073748cd2534ee
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-06-21 12:34:53 -0300

    First draft with minimum inicial setup

---
+++ lib/joke-flavor-card.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class JokeFlavorCard extends StatelessWidget {
+  final String _flavor;
+  final String _imagePath;
+
+  const JokeFlavorCard(this._flavor, this._imagePath);
+
+  @override
+  Widget build(BuildContext context) {
+    print(_flavor);
+    print(_imagePath);
+    return Card(
+        child: ListTile(
+          onTap: () {
+            print('teste');
+          },
+      leading: CircleAvatar(
+        backgroundImage: AssetImage('assets/animals.webp'),
+      ),
+      title: Text(_flavor),
+      subtitle: Text(_flavor),
+      trailing: Icon(Icons.more_vert),
+    ));
+  }
+}
